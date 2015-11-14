6e15d0c43b542e64a91b2a15f3f3e968762c5d82
charlesgriffiths <cs.griffiths@yahoo.com>
2015-11-14 20:53:23 +0900

    added license
    adopted a similar license as C version by Melissa O'Neill
    <oneill@pcg-random.org>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pcg-java
 rng permuted congruential generator http://www.pcg-random.org/
+
+The algorithm author's source at https://github.com/imneme/pcg-cpp shows an Apache License v2.0 and I am releasing this Java version under a similar license.
+
--- src/pcg/PCGRandom.java
+++ src/pcg/PCGRandom.java
@@ -2,6 +2,36 @@ package pcg;
 
 import java.util.Random;
 
+/*
+ * PCG Random Number Generation for Java.
+ *
+ * Copyright 2015 Charles Griffiths <cs.griffiths@yahoo.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For additional information about the PCG random number generation scheme,
+ * including its license and other licensing options, visit
+ *
+ *       http://www.pcg-random.org
+ */
+
+/*
+ * This code is derived from the basic C implementation by
+ * Melissa O'Neill <oneill@pcg-random.org>, which is derived
+ * from the full C implementation, which is in turn derived from the
+ * canonical C++ PCG implementation. The C++ version has many additional
+ * features and is preferable if you can use C++ in your project.
+ */
 
 public class PCGRandom extends Random
 {
