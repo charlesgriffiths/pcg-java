42177cab686c3a7a6dd9bb8d9857986070ed8f79
charlesgriffiths <cs.griffiths@yahoo.com>
2015-11-18 20:43:00 +0900

    RNGStream is an RNG
    RNGStream implements ISeekableRNG so it can be used as a source

--- src/pcg/rng/RNGStream.java
+++ src/pcg/rng/RNGStream.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 
 
-public class RNGStream extends InputStream
+public class RNGStream extends InputStream implements ISeekableRNG
 {
 ISeekableRNG source;
 byte buffer[];
@@ -98,6 +98,103 @@ RNGStream mark;
     position = (int) (amount % buffer.length);
 
     return n;
+  }
+
+
+  @Override
+  public void setState( byte[] b )
+  {
+    source.setState( b );
+  }
+
+
+  @Override
+  public void seek( long position )
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+
+  @Override
+  public void advance( long amount )
+  {
+    skip( amount );
+  }
+
+
+  @Override
+  public int blockSize()
+  {
+    return 1;
+  }
+
+
+  @Override
+  public long skipBytes( long amount )
+  {
+    return skip( amount );
+  }
+
+
+  @Override
+  public byte next8()
+  {
+  byte b = 0;
+
+    try
+    {
+      b = (byte) read();
+    }
+    catch (IOException e)  // exception not possible
+    {
+      e.printStackTrace();
+    }
+
+    return b;
+  }
+
+
+  @Override
+  public short next16()
+  {
+    return (short) (((int)next8() << 8) | ((int)next8() & 0xff));
+  }
+
+
+  @Override
+  public int next32()
+  {
+    return ((int)next16() << 16) | ((int)next16() & 0xffff);
+  }
+
+
+  @Override
+  public long next64()
+  {
+    return ((long)next32() << 32) | ((long)next32() & 0xffffffff);
+  }
+
+
+  @Override
+  public void next( byte[] b )
+  {
+    next( b, 0, b.length );
+  }
+
+
+  @Override
+  public void next( byte[] b, int offset, int length )
+  {
+    for (int i=0; i<length; i++)
+      b[i+offset] = next8();
+  }
+
+
+  @Override
+  public ISeekableRNG deepCopy()
+  {
+    return null;
   }
 }
 
